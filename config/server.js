1f00f2ad56d9f324373bbda6e25ee82d338b5484
webmatters <andrew.smith@webmatters.io>
2021-06-13 10:52:26 -0400

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2b34b6b442387168c88d7b592fa010c7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c90c8cb1-2d8b-4aae-8d05-0f6f19a2cf76'
+};
